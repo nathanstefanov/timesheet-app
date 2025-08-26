e519e6f5747b2d4d460eae7bd068fafcd236318c
Nathan Stefanov <nathanstefanov100@gmail.com>
2025-08-26 12:23:17 -0500

    Update admin dashboard and CSS fixes

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,12 +15,3 @@ export default class MyDocument extends Document {
     );
   }
 }
-// inside pages/_app.tsx, above the return:
-import Head from 'next/head';
-
-// …inside your App component's return, at the very top:
-<Head>
-  {/* Start a little zoomed-out, but DO NOT lock the zoom */}
-  <meta name="viewport" content="width=device-width, initial-scale=0.85, user-scalable=yes, viewport-fit=cover" />
-  <meta name="format-detection" content="telephone=no" />
-</Head>
